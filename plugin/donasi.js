c8c22dbc7123b25708314c683e96177423ff05fb
rielXhan <101050658+rielXhan@users.noreply.github.com>
2022-03-07 00:56:44 +0800

    Update and rename plugins/donasi.js to plugin/donasi.js

--- plugin/donasi.js
+++ plugin/donasi.js
@@ -3,17 +3,17 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
 ┌〔 Donasi • Pulsa 〕
-├ Axis🗿: 083813417529
-├ Telkomsel🗿: 082146218274
+├ Axis🗿: 6283139426854
+├ Telkomsel🗿: --
 ├ DONASI BG, BIAR GW SEMANGAT🗿
 ├ GROUP RIEL BOT¹🥀
 ├ https://chat.whatsapp.com/HYr6cgrs7jmEmPMFtwviHW
 ├ GROUP RIEL BOT²✨
 ├ https://chat.whatsapp.com/F4mwyaFGBBR9UD4LFL3P1I
-├ GROUP BOT LAN¹🥀
-├ https://chat.whatsapp.com/BCkOrQkTa7pIORSOWyBJpk
-├ GROUP BOT² LAN✨
-├ https://chat.whatsapp.com/GzDQlgoUamS5Ok4EOcYRRB
+├ GROUP BOT han¹🥀
+├ https://chat.whatsapp.com/H4pPY0xStH0AIKRZjNrx8z
+├ GROUP BOT² han²✨
+├ https://chat.whatsapp.com/H4pPY0xStH0AIKRZjNrx8z
 ├ JIKA LINK GRUB DI RESET SILA CHAT OWNER,KETIK #owner
 └────
 `.trim(), watermark, 'Menu', '.menu', m)
